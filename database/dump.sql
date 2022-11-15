da09d6089364d761b6096af6e3d8b39463a6a334
Léa M <mhoumadi.lea@gmail.com>
2022-11-15 14:57:48 +0100

    build: architecture

---
+++ database/dump.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id        INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username  VARCHAR(255) NOT NULL,
+    password  VARCHAR(255) NOT NULL,
+    admin     BOOLEAN      NOT NULL DEFAULT '0'
+);
+
+CREATE TABLE IF NOT EXISTS posts
+(
+    id      INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    content TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    user_id INT NOT NULL
+);
+
+ALTER TABLE posts ADD CONSTRAINT posts_user_id_foreign FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
